b735d32b63e1c8f9c78e55f3b486868d9e6e96d6
Heran Patel <heranp@uci.edu>
2018-12-13 18:12:22 -0800

    added latest backup

--- contracts/WhoWasAtUCIBlockathonOct2018.sol
+++ contracts/WhoWasAtUCIBlockathonOct2018.sol
@@ -3,9 +3,8 @@ pragma solidity ^0.4.24;
 contract WhoWasAtUCIBlockathonOct2018 {
 
   string[150] names;
-  uint index = 0;
+  uint public index = 0;
 
-  function recordName(string name) {
-    
-  }
+  
+
 }
